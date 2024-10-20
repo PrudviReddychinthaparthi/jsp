bb5f4f215dbb7c87b8317bf2a57f5c6c9dd401dd
PrudviReddychinthaparthi <prudvireddycsk143@gmail.com>
2024-10-20 11:59:18 +0530

    EL function

---
+++ JSPApp15-EL/WebContent/el_test.jsp
@@ -0,0 +1,3 @@
+<%@ taglib uri="http://prudvi.com/elfx" prefix="nit" %>
+
+Wish message :: ${nit:show("Raja")}
---
+++ JSPApp15-EL/src/com/nt/el/WishMessageGenerator.java
@@ -0,0 +1,9 @@
+package com.nt.el;
+
+public class WishMessageGenerator {
+	
+	public  static String generate(String user) {
+		return  "Hello, "+user;
+	}
+
+}
